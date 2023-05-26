eb31e6815d772ec34daa0da32bf8ab52dfb6ded9
Akshay Sharma <codewithakki@gmail.com>
2023-05-26 11:19:58 +0530

    folder structure

--- pages/Akshay/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Index() {
-  return (
-    <div>HEy Akshay Here let goo</div>
-  )
-}
-
-export default Index
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 import {Container }from '@mui/material'
 import { ThemeProvider } from '@mui/material';
 import theme from "./assets/theme";
