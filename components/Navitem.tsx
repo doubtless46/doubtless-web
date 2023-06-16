e31bcee83c1a3320ed2b6ca1997b49781c83c46c
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-16 15:26:19 +0530

    navbar updates

--- components/Navitem.tsx
+++ components/Navitem.tsx
@@ -6,18 +6,21 @@ interface Props{
 }
 const Navitems = (props: Props) => {
   return (
-      <Box sx={{pl:'0.5em',"&:hover": {
-        borderBottom:'2px solid black'
+      <Box sx={{mx:'0.5em',my:'0.5em',"&:hover": {
+        borderBottom:'1.5px solid #D3D3D3'
       }}} key={props.index}>
       {/* give divs id matching to the array name in the array above */}
      <Typography component="a" href={`${props.item}`} sx={{
-      textAlign:'center',
-       mr: 2,
-       mb:'10',
+      mx:'auto',
+       textAlign:'center',
+       mb:'1em',
        fontWeight: 700,
        letterSpacing: '.3rem',
        color: 'black',
        textDecoration: 'none',
+       "&:hover":{
+        color:'grey'
+       }
      }}>
        {props.item}
      </Typography>
