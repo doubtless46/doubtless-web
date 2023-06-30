0f9427e31c704ee4df5c1b1f5d7775a5cf703897
Yash-sudo-web <ymathur123@gmail.com>
2023-06-30 14:07:59 +0530

    updated tags

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -3,35 +3,35 @@ import './Tags.css'
 const Tags = () => {
   return (
     <>
-        <div className="frameButton frame-5">
-              <div className="textButton text-wrapper-8">Notes</div>
+            <div className="frameButton frame-5" style={{cursor:'pointer'}}>
+              <div style={{cursor:'pointer'}} className="textButton">Notes</div>
             </div>
-            <div className="frame-6">
+            <div className="frame-6" style={{cursor:'pointer'}}>
               <div className="frameButton frame-7">
-                <div className="textButton text-wrapper-9">Teachers</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Teachers</div>
               </div>
               <div className="frameButton frame-8">
-                <div className="textButton text-wrapper-10">Attendance</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Attendance</div>
               </div>
               <div className="frameButton frame-9">
-                <div className="textButton text-wrapper-11">Marks</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Marks</div>
               </div>
               <div className="frameButton frame-10">
-                <div className="textButton text-wrapper-12">Higher Studies</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Higher Studies</div>
               </div>
             </div>
-            <div className="frame-11">
+            <div className="frame-11" style={{cursor:'pointer'}}>
               <div className="frameButton frame-12">
-                <div className="textButton text-wrapper-13">Placements</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Placements</div>
               </div>
               <div className="frameButton frame-13">
-                <div className="textButton text-wrapper-14">Resume Review</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Resume Review</div>
               </div>
               <div className="frameButton frame-14">
-                <div className="textButton text-wrapper-15">Freelance</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Freelance</div>
               </div>
               <div className="frameButton frame-15">
-                <div className="textButton text-wrapper-16">Life</div>
+                <div className="textButton" style={{cursor:'pointer'}}>Life</div>
               </div>
             </div>
             <a href="https://play.google.com/store/apps/details?id=com.doubtless.doubtless" target="_blank" rel="noopener noreferrer">
