4a647a826937eddac11a038a2916a94215349725
Akshay Sharma <codewithakki@gmail.com>
2023-07-15 23:37:12 +0530

    limimt

--- src/functions/getQuesData.js
+++ src/functions/getQuesData.js
@@ -1,10 +1,11 @@
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs,query,limit } from "firebase/firestore";
 
 async function getFirebaseData() {
       const db = getFirestore();
       const dataRef = collection(db, "AllDoubts"); // Replace "your_collection_name" with the actual name of your collection
       try {
-        const snapshot = await getDocs(dataRef);
+        const q = query(dataRef, limit(6)); // Add the limit to the query
+        const snapshot = await getDocs(q);
         const data = snapshot.docs.map((doc) => doc.data());
       //   console.log('data',data)
         return data;
