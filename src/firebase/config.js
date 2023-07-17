6b21f9fbab206ce8cae445632beab6054ba3d8ff
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-07-17 10:50:39 +0530

    changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 
 // Your web app's Firebase configuration
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const auth=getAuth(app)
 export const AuthProvider=new GoogleAuthProvider();
 
