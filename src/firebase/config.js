b211ad4b00a2d14f6e78449f590c8849435f0324
Yash-sudo-web <ymathur123@gmail.com>
2023-07-24 23:11:33 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA75rabFGmFd5SCNyWYc89Ny4B5BZRal_4",
-  authDomain: "doubtlesstest.firebaseapp.com",
-  projectId: "doubtlesstest",
-  storageBucket: "doubtlesstest.appspot.com",
-  messagingSenderId: "222602319674",
-  appId: "1:222602319674:web:dc015c19d278305173e203",
-  measurementId: "G-8T6161KCGE"
+  apiKey: import.meta.env.VITE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGESENDERID,
+  appId: import.meta.env.VITE_APP_APPID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
